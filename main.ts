adea6de2b82cb970adf4922e24837d0fa9643788
Fayzul <fayzulhaq0708@gmail.com>
2023-11-22 20:58:59 +0800

    first commit

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import { Lexer,Parser, CodeGenerator, Evaluator } from "./source";
 
 
-const inputCode = '3 + 4 * 2 - 7 / 3';
+// const inputCode = '3 + 4 * 2 - 7 / 3';
+const inputCode = '0 + 4 * 0 - 7 / 3 + 89';
 const lexer = new Lexer(inputCode);
 const parser = new Parser(lexer);
 const tokens = parser.parse();
